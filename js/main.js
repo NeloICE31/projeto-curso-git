e9bc23bd03e4db0dd342d13be7db98c473fce4d8
NeloICE31 <danielmonteirofilho@hotmail.com>
2020-07-21 19:07:30 -0300

    lembrar de mudar para "apenas pra demostraçao"

--- js/main.js
+++ js/main.js
@@ -1,7 +1,25 @@
+/*
 var nome = "macarrao";
 var numero = 10
 var numero2 = 44
 alert(numero + numero2);
 console.log(nome);
-console.log(idade + idade2);
-//ff
+console.log(numero + numero2);
+*/
+/*var lista = ["macarrao", "peixe", "linguado"];
+//lista.pop()
+console.log(lista);
+console.log(lista.toString());
+console.log(lista.join(" ~~ "));
+//lista.push("bee boo");
+//console.log(lista.reverse());
+//////////-''resverse'' é pra botar a lista em decrecente
+//console.log(lista.length);
+//////////-''lenght'' é pra monstrar quantos na lista
+///alert(lista[1])
+*/
+/*
+var misturas = [{nome:"camarao", cor:"rosa"},{nome:"humano", cor:"arco-iris"}]
+console.log(misturas);
+alert(misturas[1].cor);
+*/
